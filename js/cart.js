48fb123e52f2a416d3a9e05860b989f0cc85cf43
Ivangyurcovits <68866090+Ivangyurcovits@users.noreply.github.com>
2020-10-11 10:06:08 -0300

    Entrega 5

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,96 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
 
+var productArray = [];
+
+function calcTotal() {
+    let subs = document.getElementsByClassName("subtotal");
+    let suma = 0;
+    for (let i = 0; i < subs.length; i++) {
+        suma += parseInt(subs[i].innerHTML)
+    }
+    document.getElementById("total").innerText = suma;
+    calcShipping();
+}
+
+function calcSubtotal(unitCost, i) {
+    let count = parseInt(document.getElementById(`count${i}`).value);
+    let subtotal = count * unitCost;
+    document.getElementById(`productSubtotal${i}`).innerHTML = subtotal;
+    calcTotal();
+}
+function showProduct(array) {
+    let content = "";
+    for (let i = 0; i < array.length; i++) {
+        let articles = array[i];
+        //let cost = coinConvert(articles.currency, articles.unitCost);
+        let unitCost = articles.unitCost;
+        let currency = articles.currency;
+        if (currency === "USD") {
+            articles.unitCost = articles.unitCost*40;
+        }
+        let sub = articles.unitCost * articles.count;
+        content += `
+        <tr>
+            <td> <img src='${articles.src}' width="50px"></td>
+            <td colspan="3"> ${articles.name}</td>
+            <td>${articles.currency}</td>
+            <td> ${unitCost}</td>
+            <td><input style="width:60px;" onchange="calcSubtotal(${articles.unitCost}, ${i})" type="number" id="count${i}" value="${articles.count}" min="1"></td>
+            <td><span class="subtotal" id="productSubtotal${i}" style="font-weight:bold;" > ${sub}</span></td>
+        </tr>
+        `
+        document.getElementById("list").innerHTML = content;
+    }
+    calcTotal();
+}
+/*function coinConvert(moneda, cantidad) {
+    switch (moneda) {
+        case "USD":
+            moneda = 40;
+            break;
+        case "UYU":
+            moneda=1;
+            break;
+    }
+    var resultado = moneda * cantidad;
+    return resultado;
+}
+*/
+function calcShipping() {
+    let total = parseInt(document.getElementById("total").innerHTML);
+    let shipping;
+
+    let elements = document.getElementsByName("shipping");
+    for (var i = 0; i < elements.length; i++) {
+        if (elements[i].checked) {
+            shipping = parseInt(elements[i].value);
+        }
+    }
+
+    let totalWithShipping = total + shipping;
+
+    let content = `
+    <tr>
+        <td>${total}</td>
+        <td>${shipping}</td>
+        <td>${totalWithShipping}</td>
+
+    </tr>
+    `
+    document.getElementById("totalShipping").innerHTML = content;
+}
+
+document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(CART_INFO_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            productArray = resultObj.data.articles;
+            showProduct(productArray);
+            calcShipping()
+        }
+    });
+    let elements = document.getElementsByName("shipping");
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].addEventListener("change", function () {
+            calcShipping()
+        });
+    }
 });
